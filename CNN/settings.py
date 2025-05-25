1a902dffcb80c6f924dbabd501d80cfc01222669
Triplea95 <odumosuadijat95@gmail.com>
2025-05-26 00:43:02 +0300

    firstcommit

--- CNN/settings.py
+++ CNN/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['my-project-24nj.onrender.com']
 
 
 # Application definition
